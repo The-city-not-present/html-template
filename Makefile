805f8eaf6c3a489f91cedc3ccf67313a5c3876bb
The-city-not-present <23407369+The-city-not-present@users.noreply.github.com>
2026-05-23 02:39:28 +0300

    Upd to build system

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
+.PHONY: build init test run
+
+
 build:
+	./build-scripts/build.sh
+
+build-only-static:
 	./build-scripts/build.sh
 
 init:
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,6 +3,12 @@ set -e
 
 # pushd ..
 
+MODE="normal"
+# Check first argument
+if [ "$1" = "only-static" ]; then
+    MODE="only-static"
+fi
+
 echo "Clear up \"compiled/\"..."
 
 mkdir -p compiled
@@ -33,30 +39,32 @@ echo "done"
 echo -
 echo -
 
-echo "Produce \"html_bundle.py\""
-echo "pre-make compiled html template within src folder"
-if [ ! -d "./src/TEMPLATE_COMPILED" ]; then
-  mkdir -p "./src/TEMPLATE_COMPILED"
+if [ "$MODE" != "debug" ]; then
+  echo "Produce \"html_bundle.py\""
+  echo "pre-make compiled html template within src folder"
+  if [ ! -d "./src/TEMPLATE_COMPILED" ]; then
+    mkdir -p "./src/TEMPLATE_COMPILED"
+  fi
+  python build.py --program build --resource "src_template" --dest "./src/TEMPLATE_COMPILED/"
+  echo "Calling pinliner..."
+  # if [ ! -f "src-make/lib/pinliner/pinliner/pinliner.py" ]; then
+  #   # TODO: confirm is having --remote fine? I think it is. It's something like apt update, it is normal to run this occasionally. I don't see an issue
+  #   git submodule update --init --recursive --remote
+  # fi
+  # comment: please delete .pyc files before every call of the html_bundle - this is implemented in my fork of the pinliner
+  # python src-make/lib/pinliner/pinliner/pinliner.py src -o compiled/html_bundle.py --verbose
+  python "src-make/lib/pinliner/pinliner/pinliner.py" src -o compiled/html_bundle.py
+  echo "done"
+  echo "Patching html_bundle.py..."
+  echo "# ..." >> "compiled/html_bundle.py"
+  echo "# print('within html_bundle')" >> "compiled/html_bundle.py"
+  # no need for this, the root package is loaded automatically
+  # echo "# import html_bundle" >> "compiled/html_bundle.py"
+  echo "from src import make_html" >> "compiled/html_bundle.py"
+  echo "# make_html.main()" >> "compiled/html_bundle.py"
+  echo "# print('out of html_bundle')" >> "compiled/html_bundle.py"
+  echo "done"
 fi
-python build.py --program build --resource "src_template" --dest "./src/TEMPLATE_COMPILED/"
-echo "Calling pinliner..."
-# if [ ! -f "src-make/lib/pinliner/pinliner/pinliner.py" ]; then
-#   # TODO: confirm is having --remote fine? I think it is. It's something like apt update, it is normal to run this occasionally. I don't see an issue
-#   git submodule update --init --recursive --remote
-# fi
-# comment: please delete .pyc files before every call of the html_bundle - this is implemented in my fork of the pinliner
-# python src-make/lib/pinliner/pinliner/pinliner.py src -o compiled/html_bundle.py --verbose
-python "src-make/lib/pinliner/pinliner/pinliner.py" src -o compiled/html_bundle.py
-echo "done"
-echo "Patching html_bundle.py..."
-echo "# ..." >> "compiled/html_bundle.py"
-echo "# print('within html_bundle')" >> "compiled/html_bundle.py"
-# no need for this, the root package is loaded automatically
-# echo "# import html_bundle" >> "compiled/html_bundle.py"
-echo "from src import make_html" >> "compiled/html_bundle.py"
-echo "# make_html.main()" >> "compiled/html_bundle.py"
-echo "# print('out of html_bundle')" >> "compiled/html_bundle.py"
-echo "done"
 echo -
 echo -
 python build.py --program done
